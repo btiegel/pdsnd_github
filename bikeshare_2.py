e2f7dfe3d0cad45ca722da89a4b15e9f44580078
be.ti@arcor.de <be.ti@arcor.de>
2021-08-20 11:11:20 +0200

    delete useless comment

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 import pandas as pd
 import datetime as dt
-#python "C:\Users\Benedikt Tiegel\python\template\bikeshare_2.py"
+
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
